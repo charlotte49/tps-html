06bdab0750cd66ef881c322001da1fef20cfd5d2
charlotte49 <charlotte.menu@gmail.com>
2021-09-30 17:29:34 +0200

    js

---
+++ js-commencement/TP1.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 15;
+const somme = a + b;
+alert("Bienvenue sur la page de formulaire");
+console.log(a, b);
+let presentation = `le resultat est ${somme}`;
+console.log(presentation);
---
+++ js-commencement/fichier.js
@@ -0,0 +1,37 @@
+let firstName1 = "Jean";
+console.log(firstName1);
+firstName1 = "Jonathan";
+console.log(firstName1);
+firstName1 = "Lydie";
+console.log(firstName1);
+
+let age2 = 67;
+age2 = "89";
+
+const accelerationPesanteur = 9.8;
+const pays = "France";
+
+const pay = "France";
+
+const lastName = "Jonathan";
+const firstName = "Jean";
+const fullName = lastName + " " + firstName;
+
+const age = 67;
+const agePere = 12;
+const ageMere = 9;
+const sommeAge = agePere + ageMere;
+
+let nombre = 45;
+let nombre2 = 26;
+let somme = nombre + nombre2;
+
+const isMarried = true;
+
+const prenom = prompt("quel est votre prenom ?");
+const nomFamille = prompt("quel est votre nom ?");
+const monAge = prompt("quel est votre age?");
+
+alert(
+  "vous etes" + prenom + "" + nomFamille + ", et vous avez" + monAge + "ans"
+);
